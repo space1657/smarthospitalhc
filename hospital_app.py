504ce42d2ca51fcb254b7bf8a82139a168e3e003
space1657 <pzyxo54@gmail.com>
2026-06-24 16:42:04 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -6,3 +6,12 @@ import os
 
 st.set_page_config(page_title="smart Hospital by chacha" , page_icon="🏥", layout="wide")
 
+st.markdown("""
+<style>
+#MainMenu { visibility: hidden; }
+header[data-testid="stHeader"] {display: none;}
+.stDeployButton {display: none;}
+footer {visibilty; hidden}
+
+</style>
+""",unsafe_allow_html=True)
