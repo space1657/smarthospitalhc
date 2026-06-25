ef62331db27c73a99bfb070b17b745bc7028aa2c
space1657 <pzyxo54@gmail.com>
2026-06-25 15:56:43 +0700

    Fix typo in model loading function

--- hospital_app.py
+++ hospital_app.py
@@ -19,7 +19,7 @@ footer {visibilty; hidden}
 @st.cache_resource
 def load_model():
   with open('hospital_model.pkl', 'rb')as f:
-    return picklr.load(F)
+    return pickle.load(F)
 
 bundel = load_model()
 model = bundel['model']
